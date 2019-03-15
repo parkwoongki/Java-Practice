ed20481303af150d42db7c906967f25129887828
parkwoongki <sbe03005@naver.com>
2019-03-15 22:01:49 +0900

    Hello JAVA World!

---
+++ Hello2030/src/Hello2030/Hello2030.java
@@ -0,0 +1,14 @@
+package Hello2030;
+
+/**
+ * Hello World 2030을 나타내는 클래스 
+ */
+public class Hello2030 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 2030;
+
+		System.out.println("Hello" + n);
+	}
+}
