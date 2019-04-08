627fb60d3556cb8d8691ee69b019df6c9fb86d53
parkwoongki <sbe03005@naver.com>
2019-04-08 17:45:43 +0900

    Use Static Example

---
+++ StaticCurrencyConverterExample/src/StaticCurrencyConverterExample/StaticCurrencyConverter.java
@@ -0,0 +1,36 @@
+package StaticCurrencyConverterExample;
+
+import java.util.Scanner;
+
+public class StaticCurrencyConverter {
+	private static double rate;
+
+	public static double toDollar(double won) {
+		return won / rate;
+	}
+
+	public static double toKWR(double dollar) {
+		return dollar * rate;
+	}
+
+	public static void setRate(double r) {
+		rate = r;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("환율(1달러)>> ");
+		double rate = scanner.nextDouble();
+		StaticCurrencyConverter.setRate(rate);
+
+		System.out.println("백만원은 $" + StaticCurrencyConverter.toDollar(1000000)
+				+ "입니다.");
+		System.out.println("$100는 " + StaticCurrencyConverter.toKWR(100)
+				+ "원입니다.");
+
+		scanner.close();
+	}
+
+}
