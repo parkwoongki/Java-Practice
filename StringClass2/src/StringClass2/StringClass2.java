6e6ff7f13887cb99b1f4420ead54d07694f8a21c
parkwoongki <sbe03005@naver.com>
2019-05-13 00:55:22 +0900

    String Class2

---
+++ StringClass2/src/StringClass2/StringClass2.java
@@ -0,0 +1,27 @@
+package StringClass2;
+
+public class StringClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		String a = new String("       oh, Happy      ");
+//		String b = a.trim();
+//		String c = b.concat(" Day.");
+//		
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+
+		String a="Hello";
+		String b="Hello";
+		String c="Java";
+		String d = new String("Hello");
+		String e = new String("Java");
+		String f = new String("Java");
+		
+		if(a==b)
+			System.out.println(true);
+	}
+
+}
