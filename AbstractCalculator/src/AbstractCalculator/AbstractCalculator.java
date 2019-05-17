9992fb1cf881e83e42b7a1c8bb6766d53abe5b8c
parkwoongki <sbe03005@naver.com>
2019-05-17 13:09:30 +0900

    Abstract Calculator

---
+++ AbstractCalculator/src/AbstractCalculator/AbstractCalculator.java
@@ -0,0 +1,17 @@
+package AbstractCalculator;
+
+abstract class Calculator {
+	public abstract int add(int a, int b);
+
+	public abstract int subtract(int a, int b);
+
+	public abstract int average(int a, int b);
+}
+
+public class AbstractCalculator {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
