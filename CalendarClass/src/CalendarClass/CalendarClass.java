52a170d28ea5de366a886b17bbb1f05610721957
parkwoongki <sbe03005@naver.com>
2019-05-17 13:13:58 +0900

    Calendar

---
+++ CalendarClass/src/CalendarClass/CalendarClass.java
@@ -0,0 +1,20 @@
+package CalendarClass;
+
+import java.util.Calendar;
+
+public class CalendarClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calendar date = Calendar.getInstance();
+		
+		date.clear();
+		date.set(Calendar.YEAR, 2020);
+		date.set(Calendar.MONTH, 11);
+		date.set(Calendar.DAY_OF_MONTH,25);
+		System.out.println("약속 날짜는 " + date.get(Calendar.YEAR)+"년 " +
+		(date.get(Calendar.MONTH)+1) + "월 "+date.get(Calendar.DAY_OF_MONTH)+"일");
+	}
+
+}
