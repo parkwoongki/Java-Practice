1e75fde54d1e1b212e3b031b6457ec5b6ac40d66
parkwoongki <sbe03005@naver.com>
2019-05-22 16:01:02 +0900

    FileReader Practice

---
+++ FileReaderEx/src/FileReaderEx/FileReaderEx.java
@@ -0,0 +1,24 @@
+package FileReaderEx;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FileReader fin = null;
+
+		try {
+			fin = new FileReader("c:\\windows\\system.ini");
+			int c;
+			while ((c = fin.read()) != -1) {
+				System.out.print((char) c);
+			}
+			fin.close();
+		} catch (IOException e) {
+			System.out.println("입출력 오류");
+		}
+	}
+
+}
